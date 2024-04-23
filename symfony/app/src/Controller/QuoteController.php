0965133a5afc5ab7426facb70bd98233f6d1cf0e
Peter Ekblom <peca23@student.bth.se>
2024-04-23 09:41:23 +0200

    Initial full commit

---
+++ symfony/app/src/Controller/QuoteController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuoteController extends AbstractController
+{
+    #[Route("/api/quote", name: "api_quote")]
+    public function quote(): JsonResponse
+    {
+
+        $quotes = [
+            "Life is to important to take seriously.",
+            "Dont wait for tomorrow",
+            "Work hard, play hard"
+        ];
+
+
+        $randomQuote = $quotes[array_rand($quotes)];
+
+
+        $date = new \DateTime("now", new \DateTimeZone('Europe/Stockholm'));
+        $dateFormat = $date->format('Y-m-d');
+        $timeStamp = $date->format('c');
+
+
+        $data = [
+            'citat' => $randomQuote,
+            'datum' => $dateFormat,
+            'tidstampel' => $timeStamp
+        ];
+
+
+        return new JsonResponse($data);
+    }
+}
